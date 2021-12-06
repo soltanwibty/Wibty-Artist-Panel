9afe34a87c1f1114ab0164147f65d50981ced4fe
soltanwibty <90504125+soltanwibty@users.noreply.github.com>
2021-12-06 21:38:01 +0400

    Page Completing

--- routes/page_routes.js
+++ routes/page_routes.js
@@ -11,11 +11,6 @@ page_router.get('/user/profile', async(req, res) => {
     return res.render('artist-profile');
 });
 
-// Adding New Album Page
-// page_router.get('/music/new', async(req, res) => {
-//     return res.render('new-music');
-// });
-
 // All Musics Statistics
 page_router.get('/music/stats/all', async(req, res) => {
     return res.render('all-music-stats');
@@ -31,5 +26,29 @@ page_router.get('/sales/stats', async(req, res) => {
     return res.render('sales-statistics');
 });
 
+// All Sale Statistics
+page_router.get('/user/edit', async(req, res) => {
+    return res.render('edit-profile');
+});
+
+// Handling All Error Pages
+
+// =-> 400
+page_router.use(function(req, res, next) {
+    res.status(400);
+    return res.render('errors/problem', { statusCode: 400, message: 'Xəta Yarandı', layout: false });
+});
+
+// =-> 403
+page_router.use(function(req, res, next) {
+    res.status(403);
+    return res.render('errors/problem', { statusCode: 403, message: 'İcazə Yoxdur', layout: false });
+});
+
+// =-> 404
+page_router.use(function(req, res, next) {
+    res.status(404);
+    return res.render('errors/problem', { statusCode: 404, message: 'Sorğu Tapılmadı', layout: false });
+});
 
 module.exports = { page_router };
