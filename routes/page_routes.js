207041e50638283d5272b7b429a75906e4f6895c
soltanwibty <90504125+soltanwibty@users.noreply.github.com>
2021-12-07 00:44:43 +0400

    Page Processing

--- routes/page_routes.js
+++ routes/page_routes.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const page_router = express.Router();
 
+// Home Page
+page_router.get('/', async(req, res) => {
+    return res.redirect('/user/profile');
+});
+
 // Login Page
 page_router.get('/login', async(req, res) => {
     return res.render('login', { layout: false });
@@ -11,9 +16,19 @@ page_router.get('/user/profile', async(req, res) => {
     return res.render('artist-profile');
 });
 
+// User Edit Page
+page_router.get('/user/edit', async(req, res) => {
+    return res.render('edit-profile');
+});
+
 // All Musics Statistics
 page_router.get('/music/stats/all', async(req, res) => {
     return res.render('all-music-stats');
+});
+
+// Music Edit Page
+page_router.get('/music/edit', async(req, res) => {
+    return res.render('music-edit');
 });
 
 // All Albums Statistics
@@ -21,14 +36,14 @@ page_router.get('/album/stats/all', async(req, res) => {
     return res.render('all-album-stats');
 });
 
-// All Sale Statistics
-page_router.get('/sales/stats', async(req, res) => {
-    return res.render('sales-statistics');
+// Album Edit Page
+page_router.get('/album/edit', async(req, res) => {
+    return res.render('album-edit');
 });
 
 // All Sale Statistics
-page_router.get('/user/edit', async(req, res) => {
-    return res.render('edit-profile');
+page_router.get('/sales/stats', async(req, res) => {
+    return res.render('sales-statistics');
 });
 
 // Handling All Error Pages
