8ad1ecb4678dd8f118b98e47390e5b9eb3cab7a1
soltanwibty <90504125+soltanwibty@users.noreply.github.com>
2021-12-06 19:20:48 +0400

    Commit

--- routes/page_routes.js
+++ routes/page_routes.js
@@ -3,12 +3,22 @@ const page_router = express.Router();
 
 // Login Page
 page_router.get('/login', async(req, res) => {
-    return res.render('login', { latout: false });
+    return res.render('login', { layout: false });
 });
 
 // Artist Profile Page
-page_router.get('/artist-profile', async(req, res) => {
+page_router.get('/user/profile', async(req, res) => {
     return res.render('artist-profile');
+});
+
+// Adding New Album Page
+page_router.get('/music/new', async(req, res) => {
+    return res.render('new-music');
+});
+
+// Musics Statistics
+page_router.get('/music/stats/all', async(req, res) => {
+    return res.render('all-music-stats');
 });
 
 module.exports = { page_router };
